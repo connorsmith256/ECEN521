0385d07edc4269fa5229dd128e28ffc9292a11e7
jmatthed <mdjames@byu.edu>
2015-10-16 12:09:24 -0600

    main.c: Reads in a file with multiple lines containing integer values separated by spaces. These values are put into a dynamically allocated array. An array for each line is passed to pancake sort, which will print the correct flip sequence used to sort the values. Added the test data file.

--- PancakeSort/PancakeSort/main.c
+++ PancakeSort/PancakeSort/main.c
@@ -10,6 +10,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Holds the pointer to our dynamically allocated array.
+int *inputArray;
+int initialSize = 10;
+
+void ReadFile(FILE* f);
+void ReadLine(char* line);
+void ExpandArray(int *oldArray);
+
 int main(int argc, const char * argv[]) {
 	
 	if (argc < 2)
@@ -24,11 +32,77 @@ int main(int argc, const char * argv[]) {
 		return 1;
 	}
 	
-	//Read File
+	ReadFile(f);
 	
-	//Call Pancake Sort
-	
-	//Print Sort
+	fclose(f);
 	
     return 0;
+}
+
+void ReadFile(FILE* f)
+{
+
+	char line[4096];
+	inputArray = NULL;
+	while (fgets(line,4096,f) != NULL)
+	{
+		if (inputArray != NULL)
+			free(inputArray);
+		inputArray = NULL;
+		
+		initialSize = 10;
+		inputArray = malloc(initialSize*sizeof(int));
+		
+		if (inputArray == NULL)
+		{
+			printf("Unable to allocate memory!\n");
+			return;
+		}
+		
+		ReadLine(line);
+		
+	}
+}
+
+
+void ReadLine(char* line)
+{
+	long int output;
+	int index = 1;
+	
+	while(1)
+	{
+		char* nextNum = line;
+		output = strtol(line, &nextNum, 10);
+		if (line == nextNum)
+		{
+			//Pointer didn't advance? end of string
+			index--;
+			break;
+		}
+		else if (index > initialSize)
+		{
+			ExpandArray(inputArray);
+		}
+		
+		line = nextNum;
+		
+		inputArray[index-1] = (int)output;
+		index++;
+	}
+	//Call Pancake Sort
+	PancakeSort(inputArray,index);
+}
+
+void ExpandArray(int* oldArray)
+{
+	int nextSize = initialSize*10;
+	int *nextArray = malloc(nextSize*sizeof(int));
+	for(int i = 0; i < initialSize; i++)
+	{
+		nextArray[i] = oldArray[i];
+	}
+	free(oldArray);
+	initialSize = nextSize;
+	inputArray = nextArray;
 }
